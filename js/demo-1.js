74dab69851622d104ee7db31c5fcd52064fb6f5f
Slyzer7 <dbsstudios3@yahoo.com>
2017-09-29 20:20:07 -0400

    Add files via upload

--- js/demo-1.js
+++ js/demo-1.js
@@ -1,6 +1,13 @@
 (function() {
 
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+    var width = true;
+    var height = true;
+    var header = true;
+    var canvas = true;
+    var ctx = true;
+    var points = true;
+    var target = true;
+    var animateHeader = true;
 
     // Main
     initHeader();
@@ -12,13 +19,13 @@
         height = window.innerHeight;
         target = {x: width/2, y: height/2};
 
-        largeHeader = document.getElementById('header');
-        largeHeader.style.height = height+'px';
+        header = document.getElementById("header");
+        header.style.height = height+"px";
 
-        canvas = document.getElementById('demo-canvas');
+        canvas = document.getElementById("demo-canvas");
         canvas.width = width;
         canvas.height = height;
-        ctx = canvas.getContext('2d');
+        ctx = canvas.getContext("2d");
 
         // create points
         points = [];
@@ -63,18 +70,18 @@
 
         // assign a circle to each point
         for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            var c = new Circle(points[i], 2+Math.random()*2, "rgba(255,255,255,0.3)");
             points[i].circle = c;
         }
     }
 
     // Event handling
     function addListeners() {
-        if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
+        if(!("ontouchstart" in window)) {
+            window.addEventListener("mousemove", mouseMove);
         }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
+        window.addEventListener("scroll", scrollCheck);
+        window.addEventListener("resize", resize);
     }
 
     function mouseMove(e) {
@@ -99,7 +106,7 @@
     function resize() {
         width = window.innerWidth;
         height = window.innerHeight;
-        largeHeader.style.height = height+'px';
+        header.style.height = height+"px";
         canvas.width = width;
         canvas.height = height;
     }
@@ -153,7 +160,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = "rgba(156,217,249,"+ p.active+")";
             ctx.stroke();
         }
     }
@@ -172,7 +179,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = "rgba(156,217,249,"+ _this.active+")";
             ctx.fill();
         };
     }
